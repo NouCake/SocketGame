64840bb4b3d85a7a8fdcccfb02c7bcd9b71d959c
NouCake <KevinFischer2010@gmail.com>
2018-05-10 01:08:08 +0200

    init commit with basic server structure

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+
+const clientPath = __dirname+'/../client';
+console.log(clientPath);
+const port = 12012;
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+const ioserver = socketio(server);
+
+ioserver.on('connection', (socket) => {
+    console.log('client connected');
+    socket.emit('message', "You're connected");
+});
+
+server.on('error', () => {
+    console.log('random error appeared');
+});
+
+server.listen(port, () => {
+    console.log('Server listens on port '+ port);
+});
